4a840a1245ae178fd9a5f8bb64649eccd70e2381
Ahaider60 <alihaiderizvi.you@gmail.com>
2022-02-13 17:29:09 +0500

    CREATE: Add user to firestore

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { db } from "./firebase-config";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
 function App() {
 	const [user, setUser] = useState([]);
+	const [newName, setNewName] = useState([]);
+	const [newAge, setNewAge] = useState([]);
 	const userCollectionRef = collection(db, "users");
 	console.log(userCollectionRef);
 
 	// CRUD
 
 	// 1. CREATE
+	const createUser = async () => {
+		await addDoc(userCollectionRef, { name: newName, age: Number(newAge) });
+	};
+
 	// 2. READ
 	useEffect(() => {
 		const getData = async () => {
@@ -23,8 +29,19 @@ function App() {
 	// 4. DELETE
 	return (
 		<div className='App'>
-			{user?.map((data) => (
-				<div>
+			<input
+				type='text'
+				name='username'
+				onChange={(e) => setNewName(e.target.value)}
+			/>
+			<input
+				type='number'
+				name='age'
+				onChange={(e) => setNewAge(e.target.value)}
+			/>
+			<button onClick={createUser}>Create User</button>
+			{user?.map((data, index) => (
+				<div key={index}>
 					<h1>{data.name}</h1>
 					<h2>{data.age}</h2>
 				</div>
