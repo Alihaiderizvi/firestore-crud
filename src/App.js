ac196df6fb83c7fe853476645cb79cddc5057526
Ahaider60 <alihaiderizvi.you@gmail.com>
2022-02-13 17:13:59 +0500

    read user from firestore

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import { db } from "./firebase-config";
 import { collection, getDocs } from "firebase/firestore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+	const [user, setUser] = useState([]);
 	const userCollectionRef = collection(db, "users");
 	console.log(userCollectionRef);
 
+	// CRUD
+
+	// 1. CREATE
+	// 2. READ
 	useEffect(() => {
 		const getData = async () => {
 			const data = await getDocs(userCollectionRef);
-			console.log("data", data);
+			setUser(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
 		};
 		getData();
 	}, []);
-	return <div className='App'></div>;
+
+	// 3. UPDATE
+	// 4. DELETE
+	return (
+		<div className='App'>
+			{user?.map((data) => (
+				<div>
+					<h1>{data.name}</h1>
+					<h2>{data.age}</h2>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default App;
