7f8562a717ae992fa1dfd0a40b491d6b307810c5
Ahaider60 <alihaiderizvi.you@gmail.com>
2022-02-13 17:06:32 +0500

    firebase Configuration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import { db } from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect } from "react";
+
 function App() {
+	const userCollectionRef = collection(db, "users");
+	console.log(userCollectionRef);
+
+	useEffect(() => {
+		const getData = async () => {
+			const data = await getDocs(userCollectionRef);
+			console.log("data", data);
+		};
+		getData();
+	}, []);
 	return <div className='App'></div>;
 }
 
